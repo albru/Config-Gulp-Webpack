66e6e26d2681149e96fec6d83d5c33fe3a5747f3
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-09-12 15:01:06 +0300

    gulp-webpack

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,3 @@
 import test from './test'
-import mobMenu from './mobile-menu/mobile-menu'
-import welcomeForm from './welcome-form/welcome-form'
-import tabsForm  from './tabs/tabs'
-import tabsMenuCircle from './tabs-menu-circle/tabs-menu-circle'
+
 test();
